7a4d24908758018d95026650d2c316706d66379a
Julian Dunskus <julian.dunskus@gmail.com>
2019-11-07 19:49:50 +0100

    removed setwebhook
    in the wrong place, and the server shouldn't be responsible for this anyway

--- Sources/App/Models/Telegram.swift
+++ Sources/App/Models/Telegram.swift
@@ -1,5 +1,6 @@
 import Foundation
 
+/// various pieces of data the telegram api gives us
 enum Telegram {
 	struct Update: Codable {
 		/// The update‘s unique identifier. Update identifiers start from a certain positive number and increase sequentially. This ID becomes especially handy if you’re using Webhooks, since it allows you to ignore repeated updates or to restore the correct update sequence, should they get out of order. If there are no new updates for at least a week, then identifier of the next update will be chosen randomly instead of sequentially.
@@ -73,27 +74,6 @@ enum Telegram {
 			
 			case emoji
 			case setName = "set_name"
-		}
-	}
-	
-	struct SetWebhook: Encodable {
-		/// HTTPS url to send updates to. Use an empty string to remove webhook integration
-		var url: String
-		
-		/// Maximum allowed number of simultaneous HTTPS connections to the webhook for update delivery, 1-100. Defaults to 40. Use lower values to limit the load on your bot‘s server, and higher values to increase your bot’s throughput.
-		var maxConnections: Int?
-		/**
-		List the types of updates you want your bot to receive. For example, specify [“message”, “edited_channel_post”, “callback_query”] to only receive updates of these types. See Update for a complete list of available update types. Specify an empty list to receive all updates regardless of type (default). If not specified, the previous setting will be used.
-		
-		Please note that this parameter doesn't affect updates created before the call to the setWebhook, so unwanted updates may be received for a short period of time.
-		*/
-		var allowedUpdates: [Update.Kind]?
-		
-		enum CodingKeys: String, CodingKey {
-			case url
-			
-			case maxConnections = "max_connections"
-			case allowedUpdates = "allowed_updates"
 		}
 	}
 }
